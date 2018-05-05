30a9b6c47fcd796188c7cf3cc77cbcda75a49c09
Quique <qui_per32@hotmail.com>
2018-05-06 01:14:13 +0200

    frecodecamp COMPLETED

---
+++ frecodecamp/eje88.js
@@ -0,0 +1,19 @@
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+    // Your Code Here
+    // Your Code Here
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+        return "Not Found";
+    }
+
+}
+
+// Test your code by modifying these values
+checkObj("gift");
---
+++ frecodecamp/eje89.js
@@ -0,0 +1,21 @@
+var myMusic = [{
+    "artist": ["Billy Joel", " Daft Punk"],
+    "title": ["Piano Man", "Happy"],
+    "release_year": 1973,
+    "formats": [
+        "CS",
+        "8T",
+        "LP",
+        "CD"
+    ],
+    "gold": true
+}, {
+    "artist": "Linking Park",
+    "title": "Living Texas",
+    "release_year": 2012,
+    "formats": [
+        "CS",
+        "8T",
+        "LP"
+    ]
+}];
