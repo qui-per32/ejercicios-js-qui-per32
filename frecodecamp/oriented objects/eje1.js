0b601967daae886ffdad0b5b95143c01d155c97e
Enrique <qui_per32@hotmail.com>
2018-05-09 10:10:43 +0200

    changed folder of frecodecamp. Added advanced js frecode

---
+++ frecodecamp/oriented objects/eje1.js
@@ -0,0 +1,15 @@
+var car = {
+    "wheels": 4,
+    "engines": 1,
+    "seats": 5
+};
+
+var motorBike = {
+
+    // Only change code below this line.
+
+
+    "wheels": 3,
+    "engines": 3,
+    "seats": 2
+};
